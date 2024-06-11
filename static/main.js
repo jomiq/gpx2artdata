26780d5a3de96ce4d9d2e691a52d4667419a9c58
Jon Mihkkal Inga <jon.mihkkal.inga@gmail.com>
2024-06-11 13:00:42 +0200

    generate options in js

--- static/main.js
+++ static/main.js
@@ -1,46 +1,50 @@
-let clipboard = new ClipboardJS(".btn");
-let copy_button = document.querySelector("#copy");
-clipboard.on("success", function (e) {
-  copy_button.innerHTML = "Kopierat!";
-  copy_button.classList.add("secondary");
-  e.clearSelection();
-});
-
-clipboard.on("error", function (e) {
-  console.error("Action:", e.action);
-  console.error("Trigger:", e.trigger);
-});
-
-let toggles = document.querySelectorAll(".toggle");
-
 function reset_copy_button() {
+  const copy_button = document.getElementById("copy");
   copy_button.classList.remove("secondary");
   copy_button.innerHTML = "Kopiera";
 }
 
-toggles.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    e.target.parentNode.parentNode.classList.toggle("disabled");
-    reset_copy_button();
+function add_options_to_list(list_id, data) {
+  const data_list = document.getElementById(list_id);
+  const tpl = document.getElementById("option-template");
+  data.forEach(element => {
+    const words = element.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    const opt = document.createElement("option")
+    opt.setAttribute("value", words.join(" "))
+    opt.innerHTML = ""
+    data_list.appendChild(opt)
   });
-});
+}
 
-let species = document.querySelectorAll(".species");
 
 function check_species(el) {
-  if (dictionary.includes(el.innerHTML.toLowerCase())) {
-    el.classList.remove("danger");
+  if (dictionary.includes(el.value.toLowerCase())) {
+    el.parentNode.classList.remove("danger");
   } else {
-    el.classList.add("danger");
+    el.parentNode.classList.add("danger");
   }
 }
 
+
 document.addEventListener("DOMContentLoaded", (e) => {
-  species.forEach((el) => {
+  const species_inputs = document.querySelectorAll("input.species");
+  add_options_to_list("species_list", dictionary);
+  species_inputs.forEach((el) => {
     check_species(el);
     el.addEventListener("input", (e) => {
       console.log(e);
       check_species(e.target);
+    });
+  });
+
+  const toggles = document.querySelectorAll(".toggle");
+  toggles.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.target.parentNode.parentNode.classList.toggle("disabled");
+      reset_copy_button();
     });
   });
 });
