a85efe5371cf43a4e18b98e82caa5011d220645a
Jon Mihkkal Inga <jon.mihkkal.inga@gmail.com>
2024-06-12 14:23:58 +0200

    hotfix

--- scripts/gcloud-push.sh
+++ scripts/gcloud-push.sh
@@ -6,7 +6,7 @@ if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
     source gcloud.env
     gcloud auth configure-docker ${REGION}-docker.pkg.dev
 
-    docker build --build-arg build_version=${BUILD_VERSION} --build-arg website_url=${WEBSITE_URL} -t $IMAGE_TAG -f Containerfile --platform linux/x86_64 .
+    docker build --build-arg git_hash=${BUILD_VERSION} --build-arg website_url=${WEBSITE_URL} -t $IMAGE_TAG -f Containerfile --platform linux/x86_64 .
     docker push $IMAGE_TAG
 else 
   # Uncommitted changes in tracked files
