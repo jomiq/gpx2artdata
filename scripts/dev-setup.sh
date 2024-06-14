2f044ffd437f4304f367c3b7bb0e66e407fd9cc8
Jon Mihkkal Inga <jon.mihkkal.inga@gmail.com>
2024-06-14 15:42:23 +0200

    chore: update dev-setup.sh

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -1,9 +1,31 @@
 #! /bin/bash
-# install into new venv 
-rm -rf .venv
-python -m venv .venv
-source .venv/bin/activate
+if [ ! -f local.env ] 
+then
+    echo " *** no local environment found. creating local.env *** "
+    cp local.env.example local.env
+else
+    echo "local.env found."
+fi
 
+# install into new venv
+[ -d .venv ] && echo " *** virtual environment found, torching it. *** " && rm -r .venv
+
+echo " *** creating venv *** "
+python -m venv .venv
+
+echo " *** installing [dev] group *** "
+source .venv/bin/activate
 pip install --upgrade pip
 pip install -e .[dev]
+
+echo " *** installing pre-commit hooks *** "
 pre-commit install
+
+echo
+echo " ***************  done!  **************** "
+echo " ***                                  *** "
+echo " ***  run source .venv/bin/activate   *** "
+echo " ***   and let the good times roll    *** "
+echo " ***                                  *** "
+echo " ******* e l e k t r o s l ö j d ******** "
+echo
