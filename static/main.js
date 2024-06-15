c32af2c9d44ae21eaeeefb86e81d70f25128cda2
Jon Mihkkal Inga <jon.mihkkal.inga@gmail.com>
2024-06-15 14:38:47 +0200

    update: layout and UI improvments. fixed: datalist is duplicated on each request

--- static/main.js
+++ static/main.js
@@ -1,4 +1,5 @@
-let copied_once = false;
+const COPY_SUCCESS_INNERHTML = 'Kopierat!  <i style="color: green;" id="copy-symbol" class="fa fa-copy"></i>';
+var focus_listener = false;
 
 function get_table_heading_text() {
   let cells = document.querySelectorAll("#data thead tr th:not(.control)");
@@ -17,7 +18,7 @@ function get_row_text(tr) {
   const cells = tr.querySelectorAll("input:not(.toggle)");
   let res = [];
   cells.forEach((c) => {
-    val = c.value;
+    val = c.value.trim();
     if (c.classList.contains("accuracy")) {
       val += " m";
     }
@@ -36,9 +37,8 @@ function get_table_text() {
   return res.join("\n");
 }
 
-function add_options_to_list(list_id, data) {
+function init_datalist(list_id, data) {
   const data_list = document.getElementById(list_id);
-  const tpl = document.getElementById("option-template");
   data.forEach((element) => {
     const words = element.split(" ");
     for (let i = 0; i < words.length; i++) {
@@ -52,7 +52,7 @@ function add_options_to_list(list_id, data) {
 }
 
 function validate_species(el) {
-  if (dictionary.includes(el.value.toLowerCase())) {
+  if (dictionary.includes(el.value.trim().toLowerCase())) {
     el.parentNode.classList.remove("danger");
   } else {
     el.parentNode.classList.add("danger");
@@ -65,38 +65,33 @@ function init_results() {
     return;
   }
 
-  copied_once = false;
   const species_inputs = document.querySelectorAll("input.species");
-  add_options_to_list("species_list", dictionary);
   species_inputs.forEach((el) => {
     validate_species(el);
     el.addEventListener("input", (e) => {
       validate_species(e.target);
     });
   });
-
+      
   const copy_button = document.getElementById("copy");
-  const copy_message = document.getElementById("copy-msg");
   if (copy_button != null) {
+    copy_button.setAttribute("init_innerHTML", copy_button.innerHTML)
     copy_button.addEventListener("click", (e) => {
-      let txt = get_table_text();
-      navigator.clipboard.writeText(txt);
-      copy_button.classList.replace("fa-copy", "fa-thumbs-o-up");
+      copy_button.setAttribute("current_text", get_table_text());
+      navigator.clipboard.writeText(copy_button.getAttribute("current_text"));
       copy_button.classList.add("secondary");
-      copy_message.innerHTML = "Kopierat!"
-      let step6 = document.querySelector("#step6");
-      if(step6 != null) {
-        step6.classList.remove("hidden");
+      copy_button.innerHTML = COPY_SUCCESS_INNERHTML;
+      let goto_artportalen_step = document.querySelector("#step-go-to-artporalen");
+      if(goto_artportalen_step != null) {
+        goto_artportalen_step.classList.remove("hidden");
       }
-      copied_once = true;
     });
   }
 
   function reset_copy_button() {
-    if(copied_once){
-      copy_button.classList.replace("fa-thumbs-o-up", "fa-copy");
+    if(copy_button.hasAttribute("current_text") && copy_button.getAttribute("current_text") != navigator.clipboard.readText()){
       copy_button.classList.remove("secondary");
-      copy_message.innerHTML = "Kopiera";
+      copy_button.innerHTML = copy_button.getAttribute("init_innerHTML");
     }
   }
 
@@ -121,13 +116,11 @@ function init_results() {
   const ap_link = document.getElementById("ap-link");
   if (ap_link != null) {
     ap_link.addEventListener("click", (e) => {
-      document.getElementById("easter-egg").classList.remove("hidden");
-    })
+      document.getElementById("step-easter-egg").classList.remove("hidden");
+    });
   }
-
 }
 
 document.addEventListener("htmx:afterSettle", (e) => {
-  console.log(e);
   init_results();
 });
