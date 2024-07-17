42ba273bbf45effead6ef1cd135124345bc91c45
jomik <lilla_molnet@gmail.com>
2024-07-17 22:01:46 +0200

    added complete colmap for artdata

--- src/gpx2artdata/converters.py
+++ src/gpx2artdata/converters.py
@@ -1,6 +1,6 @@
 from typing import IO
 import gpxpy
-from gpx2artdata.colmaps import DEFAULT_COLMAP
+from gpx2artdata.colmaps import DEFAULT_COLMAP, COLMAP
 
 
 def default_convert(file: IO, locale: str = "", accuracy: int = 1) -> dict:
@@ -19,3 +19,18 @@ def default_convert(file: IO, locale: str = "", accuracy: int = 1) -> dict:
         res["rows"].append(data)
 
     return res
+
+
+def advanced_convert(
+    rows: list[str], colmap: dict[str, str] = {}
+) -> list[dict[str, str]]:
+    for row in rows:
+        parts = [c.strip() for c in row["comment"].split(",")]
+        headings = []
+        for p in parts:
+            head_key = p.split(":")[0].strip().lower()
+            heading = colmap.get(head_key, COLMAP.get(head_key, None))
+            if heading:
+                row[heading] = p.split(":")[-1].strip()
+                if heading not in headings:
+                    headings += [heading]
